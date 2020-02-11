b437f00bb0982d64b26efc110a3fdc5d0cf3e7a4
GilAndrew <gilfandrew@gmail.com>
2020-02-10 17:32:33 -0700

    added test.cpp to test Multiples function

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("MULTIPLES OF N UP TO M ARE CALCULATED IN A VECTOR", "[Multiples]") {
+    std::vector<int> oneThree;
+    oneThree.push_back(1);
+    oneThree.push_back(2);
+    oneThree.push_back(3);
+    REQUIRE( Multiples(1,3) == oneThree);
+}
